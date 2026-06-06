08d727379638a0ffeae575ee418604c808c54c25
Lalocruz <goorrpt@gmail.com>
2026-06-06 22:48:21 +1000

    Update2 dashboard and candidate features

--- api/admin.py
+++ api/admin.py
@@ -154,22 +154,19 @@ class CandidateAdmin(admin.ModelAdmin):
 
     readonly_fields = ("candidate_id",)
 
-   fieldsets = (
-    ("Basic Info", {
-        "fields": (
-            "candidate_id",
-            ("first_name", "last_name"),
-            ("date_of_birth", "age", "gender", "height"),
-            "candidate_marital_status",
-            ("email", "phone"),
-            ("district", "address"),
-            ("father_full_name", "mother_full_name"),
+    fieldsets = (
+        ("Basic Info", {
+            "fields": (
+                "candidate_id",
+                ("first_name", "last_name"),
+                ("date_of_birth", "gender", "height"),
+                ("email", "phone"),
+                ("district", "address"),
+                ("father_full_name", "mother_full_name"),
             "total_siblings",
             "parent_marital_status",
-        )
-    }),
-)
-
+            )
+        }),
         ("Skills & Courses", {
             "fields": (
                 "trades_courses",
